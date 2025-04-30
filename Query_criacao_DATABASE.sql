4f2a3cdbad1286753e3c3471542643f073b25733
ThiagoHolandaRamalho <thiago.holandarm@hotmail.com>
2025-04-29 23:07:57 -0300

    Commit automático com token 29/04/2025 23:07:57

--- Query_criacao_DATABASE.sql
+++ Query_criacao_DATABASE.sql
@@ -1,4 +1,4 @@
- -- Atualizado em 29/04/2025 23:04:32 
+ -- Atualizado em 29/04/2025 23:07:57 
                   
                   
     IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'AdventureWorksDW2019')
